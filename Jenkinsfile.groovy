a44da05306ff892e13e806d780108c56a09bee83
Your Name <you@example.com>
2022-10-27 10:34:20 +0100

    Terraform Project

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent any
+    stages{
+        stage('Cloning Repo') {
+            steps {
+                git 'https://github.com/atiemwenjoseph1/multi-pipeline.git'
+            }
+        }
+        stage('Checking File') {
+            steps {
+                sh 'ls'
+                sh 'cat Jenkinsfile'
+                sh 'pwd'
+            }
+        }
+        stage('Terraform Provision') {
+            steps {
+                sh """
+                    terraform init
+                    terraform plan
+                    terraform apply --auto-approve
+                """
+            }
+        }
+        stage('Time the kill?') {
+            steps {
+                input message: 'Do you want to Destroy Terraform Resources?'
+            }
+        }
+        stage('Time the kill?') {
+            steps {
+                sh 'terraform destroy --auto-approve'
+                sh 'cat terraform.tfstate'
+            }
+        }
+    }
+}
---
+++ Provider.tf
@@ -0,0 +1,13 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "registry.terraform.io/hashicorp/aws"
+#       version = "4.27.0"
+#     }
+#   }
+# }
+
+provider "aws" {
+    region = "eu-west-2"
+
+}
