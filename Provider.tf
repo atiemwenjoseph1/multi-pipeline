f733109c7fd2a46527a6cffe533201959f5391cc
atiemwenjoseph1 <115942622+atiemwenjoseph1@users.noreply.github.com>
2022-10-27 11:19:11 +0100

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "registry.terraform.io/hashicorp/aws"
-#       version = "4.27.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source = "registry.terraform.io/hashicorp/aws"
+      version = "4.36.1"
+    }
+  }
+}
 
 provider "aws" {
     region = "eu-west-2"
